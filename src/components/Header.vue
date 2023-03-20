94e10a0baf3a16383446f4031a1386bd14f6f751
Naffah Abdulla Rasheed <mail@naffah.me>
2023-03-20 18:29:03 +0500

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-form')"
+      :text="showAddForm ? 'Close' : 'Add Task'"
+      :color="showAddForm ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  // As an array or object. In object you can define the type.
+  // props: ["title"], props: {title: String}
+  props: {
+    title: {
+      type: String,
+      default: "Hello, World!",
+    },
+    showAddForm: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  emits: ["toggle-add-form"],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
